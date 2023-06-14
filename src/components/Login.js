99839cf5e753da11f3f786123586658c3ce27d8e
Suraj Verma <>
2023-06-15 00:20:11 +0530

    UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,41 @@
 import Container from "react-bootstrap/Container";
+import find from "lodash/find";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function SignIn() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onHandleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onHandleSubmit = () => {
+    if (formData.email === "" || formData.password === "") {
+      return null;
+    }
+
+    let users = JSON.parse(localStorage.getItem("users"));
+    const user = find(users, { email: formData.email });
+
+    if (user.password === formData.password) {
+      localStorage.setItem("user", JSON.stringify(user));
+      navigate("/quiz");
+    } else {
+      alert(`Invalid email and password`);
+    }
+  };
   return (
     <Container class="container">
       <Row>
@@ -13,10 +44,26 @@ function SignIn() {
             <h2>Login Form</h2>
             <Form>
               <div className="form-element">
-                <Form.Label htmlFor="inputPassword5">Mobile Number</Form.Label>
-                <Form.Control type="text" aria-describedby="mobile number" />
+                <Form.Label htmlFor="inputPassword5">Email</Form.Label>
+                <Form.Control
+                  type="text"
+                  aria-describedby="email"
+                  name="email"
+                  onChange={onHandleChange}
+                />
               </div>
-              <Button variant="primary">Submit</Button>
+              <div className="form-element">
+                <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+                <Form.Control
+                  type="text"
+                  aria-describedby="password"
+                  name="password"
+                  onChange={onHandleChange}
+                />
+              </div>
+              <Button variant="primary" onClick={onHandleSubmit}>
+                Submit
+              </Button>
             </Form>
           </div>
         </Col>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,51 @@
 import Container from "react-bootstrap/Container";
+import { useNavigate } from "react-router-dom";
+import find from "lodash/find";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { useState } from "react";
 
 function Signup() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onHandleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const onHandleSubmit = () => {
+    if (
+      formData.name === "" ||
+      formData.email === "" ||
+      formData.password === ""
+    ) {
+      return null;
+    }
+
+    let users = JSON.parse(localStorage.getItem("users"));
+    const user = find(users, { email: formData.email });
+
+    if (user) {
+      alert(`User is exit with this email ID`);
+      return;
+    }
+    if (!users) {
+      users = [];
+    }
+    users.push(formData);
+    const strData = JSON.stringify(users);
+    localStorage.setItem("users", strData);
+    navigate("/");
+  };
+  console.log("============ formsData =========", formData);
   return (
     <Container class="container">
       <Row>
@@ -14,18 +55,35 @@ function Signup() {
             <Form>
               <div className="form-element">
                 <Form.Label htmlFor="inputPassword5">Name</Form.Label>
-                <Form.Control type="text" name="name" />
+                <Form.Control
+                  type="text"
+                  name="name"
+                  onChange={onHandleChange}
+                  required
+                />
               </div>
               <div className="form-element">
                 <Form.Label htmlFor="inputPassword5">Email</Form.Label>
-                <Form.Control type="text" name="email" />
+                <Form.Control
+                  type="text"
+                  name="email"
+                  onChange={onHandleChange}
+                  required
+                />
               </div>
               <div className="form-element">
                 <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-                <Form.Control type="password" name="name" />
+                <Form.Control
+                  type="password"
+                  name="password"
+                  onChange={onHandleChange}
+                  required
+                />
               </div>
 
-              <Button variant="primary">Submit</Button>
+              <Button variant="primary" onClick={onHandleSubmit}>
+                Submit
+              </Button>
             </Form>
           </div>
         </Col>
